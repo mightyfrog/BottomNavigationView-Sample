151c1478b448547ce5cc30da1e1eaa5267a02237
mightyfrog <mightyfrog.gc@gmail.com>
2020-07-12 00:22:18 +0900

    ViewModelProvider(this) -> by viewModels()

--- app/src/main/java/org/mightyfrog/android/bottomnavigationviewsample/ui/list/ListFragment.kt
+++ app/src/main/java/org/mightyfrog/android/bottomnavigationviewsample/ui/list/ListFragment.kt
@@ -6,20 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import org.mightyfrog.android.bottomnavigationviewsample.R
 
 class ListFragment : Fragment() {
 
-    private lateinit var listViewModel: ListViewModel
+    private val listViewModel: ListViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        listViewModel = ViewModelProvider(this).get(ListViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_list, container, false)
         val textView: TextView = root.findViewById(R.id.text_list)
         listViewModel.text.observe(viewLifecycleOwner, Observer {
--- app/src/main/java/org/mightyfrog/android/bottomnavigationviewsample/ui/settings/SettingsFragment.kt
+++ app/src/main/java/org/mightyfrog/android/bottomnavigationviewsample/ui/settings/SettingsFragment.kt
@@ -6,20 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import org.mightyfrog.android.bottomnavigationviewsample.R
 
 class SettingsFragment : Fragment() {
 
-    private lateinit var settingsViewModel: SettingsViewModel
+    private val settingsViewModel: SettingsViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        settingsViewModel = ViewModelProvider(this).get(SettingsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_settings, container, false)
         val textView: TextView = root.findViewById(R.id.text_settings)
         settingsViewModel.text.observe(viewLifecycleOwner, Observer {
